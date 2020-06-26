9f310124206817ff4cd5270d08ddb51d1fb4c002
svikone <velichkovlad825@gmail.com>
2020-06-26 17:47:55 +0300

    add validation form

--- frontend/src/app/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/auth/sign-in/sign-in.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl,  } from '@angular/forms';
-import { HttpService } from '../../sevices/http.service';
-import { EApiUrls } from '../../core/enums/api-urls.enums';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { HttpService } from "../../sevices/http.service";
+import { EApiUrls } from "../../core/enums/api-urls.enums";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"],
 })
 export class SignInComponent implements OnInit {
   signInForm: FormGroup;
@@ -15,33 +16,39 @@ export class SignInComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private http: HttpService,
-    private router: Router,
+    private router: Router
   ) {
     this.signInForm = this.fb.group({
-      email : [''],
-      password : [''],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  get _email() {
+    return this.signInForm.get("email");
+  }
+  get _password() {
+    return this.signInForm.get("password");
   }
 
   onSubmitForm() {
-    const controls = this.signInForm
+    const controls = this.signInForm;
     const user = {
       email: controls.value.email,
-      password: controls.value.password
-    }
+      password: controls.value.password,
+    };
 
-    this.http.post(EApiUrls.AUTH_SIGNIN, user).subscribe((value: {token: string}) =>{
-      localStorage.setItem('token', value.token);
-      controls.reset()
-      this.router.navigate(['/main']);
-    },
-    error => {
-      // error - объект ошибки
-    });
-
+    this.http.post(EApiUrls.AUTH_SIGNIN, user).subscribe(
+      (value: { token: string }) => {
+        localStorage.setItem("token", value.token);
+        controls.reset();
+        this.router.navigate(["/main"]);
+      },
+      (error) => {
+        // error - объект ошибки
+      }
+    );
   }
-
 }
--- frontend/src/app/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/auth/sign-up/sign-up.component.ts
@@ -1,45 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl,  } from '@angular/forms';
-import { HttpService } from '../../sevices/http.service';
-import { EApiUrls } from '../../core/enums/api-urls.enums';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { HttpService } from "../../sevices/http.service";
+import { EApiUrls } from "../../core/enums/api-urls.enums";
+import { Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.scss"],
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private http: HttpService
-  ) {
-      this.signUpForm = this.fb.group({
-        name :  [''],
-        email :  [''],
-        password : [''],
-        retype_password : [''],
-      });
-    }
+  constructor(private fb: FormBuilder, private http: HttpService) {
+    this.signUpForm = this.fb.group(
+      {
+        name: ["", [Validators.required]],
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required]],
+        retype_password: ["", [Validators.required]],
+      },
+      { validator: this.passwordEqualityCheck }
+    );
+  }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  get _name() {
+    return this.signUpForm.get("name");
+  }
+  get _email() {
+    return this.signUpForm.get("email");
+  }
+  get _password() {
+    return this.signUpForm.get("password");
+  }
+  get _retype_password() {
+    return this.signUpForm.get("retype_password");
+  }
+
+  passwordEqualityCheck(group: FormGroup) {
+    let pass = group.get("password").value;
+    let confirmPass = group.get("retype_password").value;
+    return pass === confirmPass ? null : { notSame: true };
   }
 
   onSubmitForm() {
-    const controls = this.signUpForm
+    const controls = this.signUpForm;
     var data = new FormData();
-    data.append('username', controls.value.name)
-    data.append('email', controls.value.email)
-    data.append('password', controls.value.password)
+    data.append("username", controls.value.name);
+    data.append("email", controls.value.email);
+    data.append("password", controls.value.password);
 
-    this.http.post(EApiUrls.AUTH_SIGNUP,data).subscribe((value: string) =>{
-      console.log(value) 
-      controls.reset()
-
-    },
-    error => {
-      // error - объект ошибки
-    });
+    this.http.post(EApiUrls.AUTH_SIGNUP, data).subscribe(
+      (value: string) => {
+        console.log(value);
+        controls.reset();
+      },
+      (error) => {
+        // error - объект ошибки
+      }
+    );
   }
 }
